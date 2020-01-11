34371366675c55627356d753e7904287462f2022
WangXing <WangXing@example.com>
2020-01-11 11:08:52 +0800

    使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 在任意位置使用vant组件  install方法
+import 'vant/lib/index.css' // 引入vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
